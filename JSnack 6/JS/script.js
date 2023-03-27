51a9afcf5063778a0a9ed51db619f5e9d84e4b34
andreaconv <andrea2convertini@gmail.com>
2023-03-27 17:47:04 +0200

    JSnack 6

---
+++ JSnack 6/JS/script.js
@@ -0,0 +1,10 @@
+/*
+JSnack 6
+Stampa il cubo dei primi N numeri, dove N è un numero indicato dall’utente.
+*/
+
+const numeroUtente = parseInt(prompt("inserisci un numero"));
+
+let cubo = numeroUtente * numeroUtente * numeroUtente;
+
+console.log(cubo);
