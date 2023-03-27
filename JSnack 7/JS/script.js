b1b7069aa2b64522a7255f952700a473442c1adf
andreaconv <andrea2convertini@gmail.com>
2023-03-27 17:52:14 +0200

    JSnack 7

---
+++ JSnack 7/JS/script.js
@@ -0,0 +1,16 @@
+/*
+JSnack 7
+Stampa le potenze di 2 fino a 1000.
+questo snack cela un trabocchetto:  1000 non è una potenza di 2
+perciò per fermarsi prima di 1000 bisogna stampare la potenza prima di incrementarla
+perché altrimenti il controllo viene fatto dopo e si sfora
+*/
+
+let numero = 1;
+
+while (numero <= 1000){
+  /*alternativa ridotta
+  numero *= 2; */
+  numero = numero * 2;
+  console.log(numero);
+}
